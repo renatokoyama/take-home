e967e0d8d5c46cf41302fcfadcc63ae5d65579bc
Renato Koyama <renato.koyama@gmail.com>
2020-12-07 04:11:32 -0300

    fix: storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { GlobalThemeProvider } from 'src/lib/theme'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-
+import { Provider } from 'react-redux'
+import configureStore from 'src/state'
 // Init all "fas" icons for components/Icon compatibility in Storybook (react-fontawesome)
 library.add(fas)
 
@@ -26,10 +27,14 @@ export const parameters = {
   },
 }
 
+const initialState = window.initialReduxState
+const store = configureStore(initialState)
 export const decorators = [
   (Story) => (
-    <GlobalThemeProvider>
-      <Story />
-    </GlobalThemeProvider>
+    <Provider store={store}>
+      <GlobalThemeProvider>
+        <Story />
+      </GlobalThemeProvider>
+    </Provider>
   ),
 ]
